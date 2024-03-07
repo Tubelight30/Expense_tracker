31ac5ad7aa06ff5e80577587681e4d12a9212ab1
Tubelight30 <soumaygupta2004@gmail.com>
2024-03-07 11:01:04 +0530

    Add system orientation lock and update theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:expense_tracker/expenses.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 var kColorScheme =
     ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 96, 59, 181));
@@ -8,53 +9,58 @@ var darkColorScheme = ColorScheme.fromSeed(
     brightness: Brightness.dark,
     seedColor: const Color.fromARGB(255, 5, 99, 125));
 void main() {
-  runApp(
-    MaterialApp(
-      title: 'Flutter Demo',
-      darkTheme: ThemeData.dark().copyWith(
-        colorScheme: darkColorScheme,
-        cardTheme: const CardTheme().copyWith(
-          color: darkColorScheme.secondaryContainer,
-          margin: const EdgeInsets.symmetric(
-            horizontal: 16,
-            vertical: 8,
-          ),
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: darkColorScheme.primaryContainer,
-            foregroundColor: darkColorScheme.onPrimaryContainer,
-          ),
-        ),
-      ),
-      theme: ThemeData().copyWith(
-        colorScheme: kColorScheme,
-        appBarTheme: AppBarTheme().copyWith(
-          backgroundColor: kColorScheme.onPrimaryContainer,
-          foregroundColor: kColorScheme.primaryContainer,
-        ),
-        cardTheme: const CardTheme().copyWith(
-          color: kColorScheme.secondaryContainer,
-          margin: const EdgeInsets.symmetric(
-            horizontal: 16,
-            vertical: 8,
-          ),
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: kColorScheme.primaryContainer,
-          ),
-        ),
-        textTheme: ThemeData().textTheme.copyWith(
-              titleLarge: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: kColorScheme.onSecondaryContainer,
-                fontSize: 14,
-              ),
+  WidgetsFlutterBinding.ensureInitialized();
+  //! basically UI will only start in portrait mode
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((value) {
+    runApp(
+      MaterialApp(
+        title: 'Flutter Demo',
+        darkTheme: ThemeData.dark().copyWith(
+          colorScheme: darkColorScheme,
+          cardTheme: const CardTheme().copyWith(
+            color: darkColorScheme.secondaryContainer,
+            margin: const EdgeInsets.symmetric(
+              horizontal: 16,
+              vertical: 8,
             ),
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: darkColorScheme.primaryContainer,
+              foregroundColor: darkColorScheme.onPrimaryContainer,
+            ),
+          ),
+        ),
+        theme: ThemeData().copyWith(
+          colorScheme: kColorScheme,
+          appBarTheme: AppBarTheme().copyWith(
+            backgroundColor: kColorScheme.onPrimaryContainer,
+            foregroundColor: kColorScheme.primaryContainer,
+          ),
+          cardTheme: const CardTheme().copyWith(
+            color: kColorScheme.secondaryContainer,
+            margin: const EdgeInsets.symmetric(
+              horizontal: 16,
+              vertical: 8,
+            ),
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: kColorScheme.primaryContainer,
+            ),
+          ),
+          textTheme: ThemeData().textTheme.copyWith(
+                titleLarge: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: kColorScheme.onSecondaryContainer,
+                  fontSize: 14,
+                ),
+              ),
+        ),
+        //! themeMode: ThemeMode.system, //default
+        home: const Expenses(),
       ),
-      //! themeMode: ThemeMode.system, //default
-      home: const Expenses(),
-    ),
-  );
+    );
+  });
 }
